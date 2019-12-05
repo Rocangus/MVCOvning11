27f665ff83c15a0fe5b82dcaa61fc7fa69d8e137
Rocangus <hunter_sniper112@hotmail.com>
2019-12-05 09:47:33 +0100

    Created Product model

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Category { get; set; }
+        public string Shelf { get; set; }
+        public int Count { get; set; }
+        public string Description { get; set; }
+    }
+}
